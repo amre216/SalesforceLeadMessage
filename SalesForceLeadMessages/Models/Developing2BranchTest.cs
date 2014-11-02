6c1df10c121ef8d5726cb88bc7c91934a35d71d8
Amar Resic <amre216@gmail.com>
2014-11-02 16:53:28 +0100

    Added developing2Bracnhtest class

---
+++ SalesForceLeadMessages/Models/Developing2BranchTest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SalesForceLeadMessages.Models
+{
+    public class Developing2BranchTest
+    {
+        public string developingTwoBranchTest;
+
+    }
+}
