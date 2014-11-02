17c88a26afed3fcd129217d95dea204ed5089f31
Amar Resic <amre216@gmail.com>
2014-11-02 16:36:09 +0100

    Added a new test class

---
+++ SalesForceLeadMessages/Models/GitTestClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SalesForceLeadMessages.Models
+{
+    public class GitTestClass
+    {
+        string name { get; set; }
+    }
+}
